fc6245be560f3a6bc8fbc70b9f8a7ea019f3a3c3
TechApps <techappsv@hotmail.com>
2022-01-13 17:19:33 -0600

    Version 1.4.4-alpha

---
+++ src/munich/version/version.js
@@ -0,0 +1,10 @@
+// The Munich Proyect
+
+
+
+var text2 = document.createTextNode("Electron 16.0.5");                                    
+document.getElementById("electron-version-munich").appendChild(text2); 
+var text3 = document.createTextNode("1.3 Stable");                                    
+document.getElementById("munich-version").appendChild(text3); 
+var text5 = document.createTextNode(" 21 (Beta 01070122)");                                    
+document.getElementById("version").appendChild(text5); 
